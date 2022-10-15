75cfb3614046110448816440874f6574e581cb47
Chommyrita <chiomaonyekwuluje@gmail.com>
2022-10-15 06:05:21 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include <stdio>
 /**
 *main - Entry point
 *My hello, printf task
@@ -7,6 +7,6 @@
 int main(void)
 {
     printf("with proper grammer, but the outcome is a piece of art,\n");
-    return (0)
+    return (0);
 }
 

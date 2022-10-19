ec50ca50960c442fbb94665a361246fcf6196fb3
Chommyrita <chiomaonyekwuluje@gmail.com>
2022-10-19 00:28:35 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,12 @@ void times_table(void)
 			if (op <= 9)
 			{
 				_putchar(32);
-				_putchar(op % 10) + 48);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
 			}
 
 		}
